15068be6de06c2b14ad77e000bae19c39d59ace9
duduluu@outlook.com <duduluu@outlook.com>
2017-06-14 20:05:57 +0800

    Renamed

--- d.bat
+++ d.bat
@@ -1,10 +1,10 @@
-rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\About"
-rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Assemblies"
-rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Defs"
-rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Languages"
-xcopy About "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\About" /e /d /i /y
-xcopy Assemblies "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Assemblies" /e /d /i /y
-xcopy Defs "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Defs" /e /d /i /y
-xcopy Languages "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\Languages" /e /d /i /y
-copy LICENSE "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\LICENSE" /y
-copy LICENSE "D:\Game\Steam\steamapps\common\RimWorld\Mods\Translation Framework\README.md" /y
+rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\About"
+rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Assemblies"
+rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Defs"
+rd /s /q "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Languages"
+xcopy About "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\About" /e /d /i /y
+xcopy Assemblies "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Assemblies" /e /d /i /y
+xcopy Defs "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Defs" /e /d /i /y
+xcopy Languages "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\Languages" /e /d /i /y
+copy LICENSE "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\LICENSE" /y
+copy LICENSE "D:\Game\Steam\steamapps\common\RimWorld\Mods\RimWorld Translation Framework\README.md" /y
