aaa83343227fd3865b3690387fd6011377fae7d1
duduluu@outlook.com <duduluu@outlook.com>
2017-06-17 00:29:52 +0800

    Minor Config

--- Source/RimTrans.Framework.Step1/Program.cs
+++ Source/RimTrans.Framework.Step1/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace RimTrans.Framework.Step1 {
     class Program {
         static void Main(string[] args) {
+            string configFile = "Config.xml";
+            if (File.Exists(configFile)) {
+                Generate(configFile);
+            } else {
+                Console.Write("[ERROR] Config.xml no found.");
+                Console.ReadKey();
+            }
+        }
+
+        static void Generate(string configFile) {
+            XDocument configDoc = XDocument.Load(configFile);
+            string frameworkPath = configDoc.Root.Element("FrameworkPath").Value;
+            string modPath = configDoc.Root.Element("ModPath").Value;
+            string language = configDoc.Root.Element("Language").Value;
+
+            string defsFolder = Path.Combine(frameworkPath, "Defs");
+            string vanillaNamesFolder = Path.Combine(defsFolder, "VanillaNames");
+            string solidNamesFile = Path.Combine(vanillaNamesFolder, "SolidNames.xml");
+            XDocument solidNamesDoc = XDocument.Load(solidNamesFile);
         }
     }
 }
