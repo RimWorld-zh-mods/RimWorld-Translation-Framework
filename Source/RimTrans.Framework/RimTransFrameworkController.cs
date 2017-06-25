429f009b8631eb51b570157af1132a49607fec3a
duduluu@outlook.com <duduluu@outlook.com>
2017-06-25 17:29:14 +0800

    Fixed sln

--- Source/RimTrans.Framework/RimTransFrameworkController.cs
+++ Source/RimTrans.Framework/RimTransFrameworkController.cs
@@ -19,6 +19,23 @@ namespace RimTrans.Framework {
             PawnBioAndBacktoryUtility.AddAllCustom();
             ShuffledNameUtility.TranslateVanilla();
             ShuffledNameUtility.AddAllCustom();
+            CombineNameUtility.CombineAndAddToDatabase();
+
+#if DEBUG
+            // DEBUG
+            Debug();
+#endif
+        }
+
+        public static void Debug() {
+            NameBank nameBank = PawnNameDatabaseShuffled.BankOf(PawnNameCategory.HumanStandard);
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.First} {Gender.Male} {nameBank.NamesFor(PawnNameSlot.First, Gender.Male).Count}");
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.First} {Gender.Female} {nameBank.NamesFor(PawnNameSlot.First, Gender.Female).Count}");
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.Nick} {Gender.Male} {nameBank.NamesFor(PawnNameSlot.Nick, Gender.Male).Count}");
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.Nick} {Gender.Female} {nameBank.NamesFor(PawnNameSlot.Nick, Gender.Female).Count}");
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.Nick} {Gender.None} {nameBank.NamesFor(PawnNameSlot.Nick, Gender.None).Count}");
+            Log.Message($"PawnNameDatabaseShuffled {PawnNameSlot.Last} {Gender.None} {nameBank.NamesFor(PawnNameSlot.Last, Gender.None).Count}");
+            Log.Message($"PawnNameDatabaseSolid {PawnNameDatabaseSolid.AllNames().Count()}");
         }
     }
 }
